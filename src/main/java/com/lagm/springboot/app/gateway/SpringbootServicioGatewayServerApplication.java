e0ba868ed8050af9392684404f41288c145321ab
Luis Guisado <lguisadom@gmail.com>
2021-12-14 02:23:00 -0500

    Inicio

---
+++ src/main/java/com/lagm/springboot/app/gateway/SpringbootServicioGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.lagm.springboot.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioGatewayServerApplication.class, args);
+	}
+
+}
