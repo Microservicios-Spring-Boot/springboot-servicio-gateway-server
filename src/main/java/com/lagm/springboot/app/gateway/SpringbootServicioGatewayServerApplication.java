5176d2cab9c3fe12c7fca71ff342536684d9325c
Luis Guisado <lguisadom@gmail.com>
2021-12-14 03:05:13 -0500

    Configuracion de rutas y postman

--- src/main/java/com/lagm/springboot/app/gateway/SpringbootServicioGatewayServerApplication.java
+++ src/main/java/com/lagm/springboot/app/gateway/SpringbootServicioGatewayServerApplication.java
@@ -2,7 +2,11 @@ package com.lagm.springboot.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import jdk.jfr.Enabled;
+
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioGatewayServerApplication {
 
