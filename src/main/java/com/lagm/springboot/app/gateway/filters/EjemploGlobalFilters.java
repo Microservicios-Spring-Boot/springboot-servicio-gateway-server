7648ee35b15c0930aeb679b676b35230c803b7d7
Luis Guisado <lguisadom@gmail.com>
2023-04-21 02:38:30 -0500

    Implementando filtros globales pre y post

---
+++ src/main/java/com/lagm/springboot/app/gateway/filters/EjemploGlobalFilters.java
@@ -0,0 +1,30 @@
+package com.lagm.springboot.app.gateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class EjemploGlobalFilters implements GlobalFilter {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EjemploGlobalFilters.class);
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		LOGGER.info("Ejecutando filtro PRE");
+		
+		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+			LOGGER.info("Ejecutando filtro POST");
+			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
+			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+		}));
+	}
+
+}
