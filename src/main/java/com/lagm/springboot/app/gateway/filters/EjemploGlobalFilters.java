c329aacf07539079298b7c3199fb0222cc6d544c
Luis Guisado <lguisadom@gmail.com>
2023-04-21 02:52:05 -0500

    Modificando filtros globales pre y post
    El método getOrder() es una implementación de la interfaz Ordered, que es una interfaz que permite establecer un orden específico de ejecución para los filtros globales de Spring Cloud Gateway. Los filtros globales se ejecutan antes y después de enrutar una solicitud al microservicio, lo que permite realizar operaciones de pre y post procesamiento.
    
    En este caso particular, el método getOrder() devuelve -1, lo que significa que este filtro tiene una prioridad alta y se ejecutará antes que otros filtros con una prioridad menor. El orden de ejecución de los filtros globales es de menor a mayor, por lo que los filtros con un valor de retorno menor se ejecutan antes que los filtros con un valor de retorno mayor.
    
    El método getOrder() es importante en este caso porque se está modificando el valor de la cabecera de solicitud token y agregando una cookie y un tipo de contenido a la respuesta. Es importante que estas operaciones se realicen antes de que se enrute la solicitud al microservicio, de lo contrario, el microservicio podría no recibir los valores necesarios.
    
    En resumen, el método getOrder() permite establecer un orden de ejecución específico para los filtros globales en Spring Cloud Gateway, lo que es útil para garantizar que las operaciones de pre y post procesamiento se realicen en el orden correcto. En este caso, el filtro tiene una prioridad alta y se ejecutará antes que otros filtros con una prioridad menor.

--- src/main/java/com/lagm/springboot/app/gateway/filters/EjemploGlobalFilters.java
+++ src/main/java/com/lagm/springboot/app/gateway/filters/EjemploGlobalFilters.java
@@ -1,9 +1,12 @@
 package com.lagm.springboot.app.gateway.filters;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,19 +15,38 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class EjemploGlobalFilters implements GlobalFilter {
+public class EjemploGlobalFilters implements GlobalFilter, Ordered {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EjemploGlobalFilters.class);
-	
+
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		LOGGER.info("Ejecutando filtro PRE");
-		
+		LOGGER.info("Ejecutando filtro PRE: Antes de enrutar la solicitud al microservicio");
+
+		// Se simula pasar un token en la cabecera del request
+		exchange.getRequest().mutate().headers(h -> h.add("token", "12356"));
+
 		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-			LOGGER.info("Ejecutando filtro POST");
+			LOGGER.info("Ejecutando filtro POST: Después de enrutar la solicitud al microservicio");
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
+				exchange.getResponse().getHeaders().add("token", valor);
+			});
+
 			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
 			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 		}));
+	}
+
+	/*
+	 * permite establecer un orden de ejecución específico para los filtros globales
+	 * en Spring Cloud Gateway, lo que es útil para garantizar que las operaciones
+	 * de pre y post procesamiento se realicen en el orden correcto. En este caso,
+	 * el filtro tiene una prioridad alta y se ejecutará antes que otros filtros con
+	 * una prioridad menor.
+	 */
+	@Override
+	public int getOrder() {
+		return -1;
 	}
 
 }
